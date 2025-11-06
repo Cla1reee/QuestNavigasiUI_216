a7619e81092189d93f62838d9b33606b31be4c0b
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-06 18:23:26 +0700

    membuat form isian

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -1,2 +1,25 @@
 package com.example.navigasi.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    onSubmitBtnClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home)) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700),
+                    titleContentColor = Color.White
+                )
+            )
+        }
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
