5d3910f110795b8e37e2b12d10870ac1916da0a3
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-06 09:32:58 +0700

    membuat file navigasi.kt

---
+++ app/src/main/java/com/example/navigasi/navigasi.kt
@@ -0,0 +1,44 @@
+package com.example.navigasi
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.ui.navigateUp
+
+enum class Navigasi{
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+){
+    Scaffold{ isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulir.name) {
+                FormIsian (
+                    onSubmitButtonClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+
+                TampilanData(
+                    onBackButtonClick = { navController.navigateUp() }
+                )
+            }
+        }
+    }
+}
