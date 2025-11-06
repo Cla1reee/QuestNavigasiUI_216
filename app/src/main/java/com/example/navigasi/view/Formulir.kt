3ece7348a45f54d5bffe62369e1c1d31702e6dfd
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-06 18:31:03 +0700

    tambah field alamat

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -59,6 +59,14 @@ fun FormIsian(
                 thickness = 1.dp,
                 color = Color.Red
             )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = { Text(text = "Alamat") },
+                onValueChange = {},
+            )
         }
     }
 }
