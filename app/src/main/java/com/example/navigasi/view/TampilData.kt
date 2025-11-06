909c4e573bfa9682690677c30334e25b1578a8d2
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-06 18:46:56 +0700

    membuat TampilData.kt

--- app/src/main/java/com/example/navigasi/view/TampilData.kt
+++ app/src/main/java/com/example/navigasi/view/TampilData.kt
@@ -1,2 +1,23 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.navigasi.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TampilData(
+    items: List<Pair<String, String>> = emptyList(),
+    onBackBtnClick: () -> Unit = {}
+) {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ) {
+    }
+}
